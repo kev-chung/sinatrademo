86ffdda90c2bb57d9a97c1245c65bce0c6d82eeb
Kevin Chung <kchung@Kevins-MBP.home>
2020-08-25 12:31:34 -0400

    Attempted to fix issue with deploying postres to Heroku

--- db/migrate/20200825154444_create_cakes.rb
+++ db/migrate/20200825154444_create_cakes.rb
@@ -1,9 +1,9 @@
 class CreateCakes < ActiveRecord::Migration[5.2]
   def change
   	create_table :cakes do |t|
-      t.string :name, null: false, default: ""
+      t.string :cake, null: false, default: ""
 
-      t.timestamps, null: false
+      t.timestamps null: false
     end
 
     add_index :resources, :name, unique: true
